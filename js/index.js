f6931ed4bcd7aa3037926e258553b51e43c729e9
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-01 15:19:07 +0600

    add  all detiles

--- js/index.js
+++ js/index.js
@@ -7,9 +7,12 @@ const searchBtn = () => {
 }
 
 const getValue = datas => {
-    // console.log(datas)
-    datas.forEach(data => {
-        const cardSection = document.getElementById('card-section')
+    // console.log(datas.slice(0, 20))
+    const update = datas.slice(0, 20)
+    const cardSection = document.getElementById('card-section')
+    cardSection.innerHTML = ''
+    update.forEach(data => {
+
         const div = document.createElement('div')
         div.classList.add('col-md-4')
         div.innerHTML = `
@@ -18,12 +21,16 @@ const getValue = datas => {
             <div class="card-body main-card p-4">
                 <h4 class="card-title "> Name : ${data.phone_name}</h4>
                 <h4 class="card-title "> Brand : ${data.brand}</h4>
+                
                 <button onclick="detilesBtn('${data.slug}')" style="  background-color: #c03488;" class="btn text-white  shadow-lg">Detiles</button>
             </div>
         </div>`
         cardSection.appendChild(div)
             // console.log(data)
     })
+
+
+
 }
 
 const detilesBtn = (phoneId) => {
@@ -37,15 +44,18 @@ const detilesBtn = (phoneId) => {
 
 const phoneIdDetails = (idData) => {
     const cardDetiles = document.getElementById('card-detiles')
-
+    cardDetiles.innerHTML = ''
     const div = document.createElement('div')
-    div.classList.add('card-position')
+
+    // div.classList.add('card-position')
     const feature = idData.mainFeatures
+    const others = idData.others
+    const sensors = feature.sensors
     div.innerHTML = `
-    <div class="card mb-3 border-0 d-flex justify-content-center align-items-center shadow-lg rounded-3" style="width:85%; height:500px; background-color: rgba(233, 187, 176, 0.979);">
+    <div class="card mb-3 border-0  d-flex justify-content-center align-items-center shadow-lg rounded-3" style="width:100%;  background-color: rgba(233, 187, 176, 0.979);">
     <div class="row ">
-        <div class="col-md-4 p-5">
-            <img src="${idData.image}" class="img-fluid rounded-start h-100 w-100" alt="...">
+        <div class="col-md-4 py-1 px-5 d-flex justify-content-center align-items-center">
+            <img src="${idData.image}" class="img-fluid rounded-start h-50 w-100" alt="...">
         </div>
         <div class="col-md-8">
             <div class="card-body p-5">
@@ -53,8 +63,35 @@ const phoneIdDetails = (idData) => {
                 <p class="card-text">Storage : ${feature.storage}</p>
                 <p class="card-text">Display : ${feature.displaySize}</p>
                 <p class="card-text"> Chip Set : ${feature.chipSet}</p>
-            
-                <button onclick="closeBtn()" class="btn text-white fs-5" style="  background-color: #c03488;">close</button>
+      
+                <div class="row">
+                    <div class="col-md-6">
+                    <p class="card-text">Bluetooth: ${others.Bluetooth}</p>
+                    <p class="card-text">GPS : ${others.GPS}</p>
+                    <p class="card-text"> NFC : ${others.NFC}</p>
+                    <p class="card-text"> Radio : ${others.Radio}</p>
+                    <p class="card-text"> USB : ${others.USB}</p>
+                    <p class="card-text"> WLAN : ${others.WLAN}</p>
+                  
+                    </div>
+
+
+                    <div class="col-md-6 fs-5 justify-content-end">
+                    <hr>
+                    <h4 class="card-title ">Sensor</h4>
+                                <p class="card-text"> ${sensors[0]}</p>
+                                <p class="card-text"> ${sensors[1]}</p>
+                                <p class="card-text"> ${sensors[2]}</p>
+                                <p class="card-text"> ${sensors[3]}</p>
+                                <p class="card-text"> ${sensors[4]}</p>
+                                <p class="card-text"> ${sensors[5]}</p>
+                               
+                    </div>
+                </div>
+                
+             
+                <button onclick="closeBtn()" class="btn text-white " style="  background-color: #c03488;">close</button>
+              
                 
             </div>
         </div>
