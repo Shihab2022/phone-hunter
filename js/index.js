a402f71038c5a75460f5fa8a6c8245776f55ad8f
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-02 08:16:48 +0600

    add error condition

--- js/index.js
+++ js/index.js
@@ -1,31 +1,51 @@
 const searchBtn = () => {
     const getInput = document.getElementById('get-input').value
+
     fetch(`https://openapi.programming-hero.com/api/phones?search=${getInput}`)
         .then(res => res.json())
-        .then(data => getValue(data.data))
-
+        .then(data => getValue(data))
+    document.getElementById('get-input').value = ''
 }
 
+
+// const errrorMessage = (error) => {
+//     console.log('HELLO')
+// }
+
+
 const getValue = datas => {
-    const update = datas.slice(0, 20)
+
+    const update = datas.data.slice(0, 20)
     const cardSection = document.getElementById('card-section')
     cardSection.innerHTML = ''
-    update.forEach(data => {
 
-        const div = document.createElement('div')
-        div.classList.add('col-md-4')
-        div.innerHTML = `
-            <div class="card shadow-lg rounded-3  border-0 my-4 " style="width: 98%;height:70%;">
-            <img src="${data.image}" class="card-img-top" alt="...">
-            <div class="card-body main-card d-flex justify-content-center flex-column p-4">
-                <h4> Name : ${data.phone_name}</h4>
-                <h4> Brand : ${data.brand}</h4>
-                
-                <button onclick="detilesBtn('${data.slug}')" style="  background-color: #c03488;" class="btn py-2 my-2  text-white  shadow-lg">Detiles</button>
-            </div>
-        </div>`
-        cardSection.appendChild(div)
-    })
+    if (!datas.status) {
+        cardSection.innerHTML = `
+        <h1 class="text-center text-danger">Sorry , Phone is not found !!!!!</h1>
+        `
+    }
+    // console.log(datas.status)
+    else {
+        update.forEach(data => {
+
+            const div = document.createElement('div')
+            div.classList.add('col-md-4')
+            div.innerHTML = `
+                <div class="card shadow-lg rounded-3  border-0 my-4 " style="width: 98%;height:70%;">
+                <img src="${data.image}" class="card-img-top" alt="...">
+                <div class="card-body main-card d-flex justify-content-center flex-column p-4">
+                    <h4> Name : ${data.phone_name}</h4>
+                    <h4> Brand : ${data.brand}</h4>
+                    
+                    <button onclick="detilesBtn('${data.slug}')" style="  background-color: #c03488;" class="btn py-2 my-2  text-white  shadow-lg">Detiles</button>
+                </div>
+            </div>`
+            cardSection.appendChild(div)
+        })
+
+    }
+
+
 }
 
 const detilesBtn = (phoneId) => {
@@ -42,6 +62,7 @@ const phoneIdDetails = (idData) => {
     const div = document.createElement('div')
     const feature = idData.mainFeatures
     const others = idData.others
+    console.log(idData.releaseDate)
     div.innerHTML = `
     <div class="card mb-3 border-0  d-flex justify-content-center align-items-center shadow-lg rounded-3" style="width:98%;  background-color: rgba(233, 187, 176, 0.979);">
         <div class="row ">
@@ -50,7 +71,7 @@ const phoneIdDetails = (idData) => {
              </div>
             <div class="col-md-7">
             <div class="card-body p-5">
-                <h3 class="card-title">Release : ${idData?.releaseDate ?? 'Release date is not found'}</h3>
+                <h3 class="card-title">Release : ${idData.releaseDate || 'Release date is not found'}</h3>
                 <p>Storage : ${feature.storage}</p>
                 <p>Display : ${feature.displaySize}</p>
                 <p> Chip Set : ${feature.chipSet}</p> 
@@ -65,15 +86,18 @@ const phoneIdDetails = (idData) => {
                     <h5>USB : <span>${others?.USB ?? 'This value is not found'}</span></h5>
                     <h5>WLAN : <span>${others?.WLAN ?? 'This value is not found'}</span></h5>        
                     </div>
-                    <div class="col-md-4 fs-5 justify-content-end">
+                    <div class="col-md-4 fs-5 justify-content-center">
                    
                     <h4  class="card-title py-2">Sensor</h4>
-                                <span> ${feature?.sensors[0]}</span>
-                                <span> ${feature?.sensors[1]}</span>
-                                <span> ${feature?.sensors[2]}</span>
-                                <span> ${feature?.sensors[3]}</span>
-                                <span> ${feature?.sensors[4]}</span>
-                                <span> ${feature?.sensors[5] ?? 'hello'}</span>                              
+                    <ul>     
+                    <li> ${feature?.sensors[0]}</li>
+                    <li> ${feature?.sensors[1]}</li>
+                    <li> ${feature?.sensors[2]}</li>
+                    <li> ${feature?.sensors[3]}</li>
+                    <li> ${feature?.sensors[4]}</li>
+                    <li> ${feature?.sensors[5] ?? 'hello'}</li>
+                     </ul>
+                                                        
                     </div>
                 </div>                        
                 <button onclick="closeBtn()" class="btn text-white my-3 " style="  background-color: #c03488;">close</button>       
